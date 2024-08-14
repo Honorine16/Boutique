154366642475253f84e32c279527d22fb858045b
= <abotchihonorine28@gmail.com>
2024-08-14 11:56:27 -0700

    css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,26 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <title>Authentification</title>
 </head>
 
-<body>
-    <table>
-        <tbody>
-            <tr>
-                <td>
-                    <h1>Tableau de bord</h1>
-                </td>
-                <td>
-                    <a href="{{ route('logout') }}">
-                        Se déconnecter
-                    </a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <h3>
-        bienvenue {{ Auth::user()->name }}
-    </h3>
+<body class="BigContainer">
+    <div class="container">
+        <table>
+            <tbody>
+                <tr class="cont">
+                    <td>
+                        <h1>Tableau de bord</h1>
+                    </td>
+                    <td>
+                        <a href="{{ route('logout') }}">
+                            Se déconnecter
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <h3>
+            bienvenue {{ Auth::user()->name }}
+        </h3>
+    </div>
 </body>
 </html>
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -2,42 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Authentification</title>
 </head>
 
-<body>
-    <form action="{{ route('registration.process') }}" method="POST">
-        @csrf
-        <h1>Inscription</h1>
-
-        @if ($errors->any())
-            <ul class="alert alert-danger">
-                {!! implode('', $errors->all('<li>:message</li>')) !!}
-            </ul>
-        @endif
-
-        @if ($message = Session::get('error'))
-            <div>{{ $message }}</div><br>
-        @endif
-
-        <label for="name">Nom d'utilisateur</label><br>
-        <input type="text" name="name" id="name" placeholder="Saisir le nom ici..."><br><br>
-
-        <label for="email">Email</label><br>
-        <input type="email" name="email" id="email" placeholder="Saisir l'e-mail ici..."><br><br>
+<body class="BigContainer">
+    <div class="container">
+        <form action="{{ route('registration.process') }}" method="POST">
+            @csrf
+            <h1>Inscription</h1>
     
-        <label for="password">Mot de passer</label><br>
-        <input type="password" name="password" id="password" placeholder="Saisir le mot de passe ici..."><br><br>    
+            @if ($errors->any())
+                <ul class="alert alert-danger">
+                    {!! implode('', $errors->all('<li>:message</li>')) !!}
+                </ul>
+            @endif
     
-        <label for="password">Confirmer le mot de passe</label><br>
-        <input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="Confirmer le mot de passe ici..."><br><br>    
+            @if ($message = Session::get('error'))
+                <div>{{ $message }}</div><br>
+            @endif
     
-        <a href="{{ route('login') }}">Se connecter</a><br><br>    
+            <label for="name">Nom d'utilisateur</label><br>
+            <input type="text" name="name" id="name" placeholder="Saisir le nom ici..."><br><br>
     
-            <button type="submit">Soumettre</button>
-    </form>
+            <label for="email">Email</label><br>
+            <input type="email" name="email" id="email" placeholder="Saisir l'e-mail ici..."><br><br>
+        
+            <label for="password">Mot de passer</label><br>
+            <input type="password" name="password" id="password" placeholder="Saisir le mot de passe ici..."><br><br>    
+        
+            <label for="password">Confirmer le mot de passe</label><br>
+            <input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="Confirmer le mot de passe ici..."><br><br>    
+        
+            <a href="{{ route('login') }}">Se connecter</a><br><br>    
+        
+                <button type="submit">Soumettre</button>
+        </form>
+    </div>
 </body>
 
 </html>
